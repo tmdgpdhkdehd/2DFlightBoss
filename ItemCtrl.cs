f6df75dec13ebabab92ea4ef1c91b2535c48cdc1
Ellieka <42927706+tmdgpdhkdehd@users.noreply.github.com>
2022-05-10 22:11:23 +0900

    Create ItemCtrl.cs

---
+++ ItemCtrl.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemCtrl : MonoBehaviour
+{
+    public float powerUp = 1;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "borderBullet")
+            Destroy(gameObject);
+        else if (collision.gameObject.tag == "Player")
+            Destroy(gameObject);
+    }
+}
+
