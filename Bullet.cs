b2acb4ad5dac615895a0e0cb9c14fb559ed2a16e
Ellieka <42927706+tmdgpdhkdehd@users.noreply.github.com>
2022-05-10 22:12:10 +0900

    Create Bullet.cs

---
+++ Bullet.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    public int damage = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "borderBullet")
+            Destroy(gameObject);
+    }
+}
